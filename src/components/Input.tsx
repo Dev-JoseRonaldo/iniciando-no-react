04840df6f5f892d4445ce33d5dc16c3073f6c927
José Ronaldo <dev.joseronaldo@gmail.com>
2022-05-19 17:09:05 -0300

    :sparkles: feat: Resolvendo o exercício do formulário

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 
 export const Input = () => {
-  const [name , setName] = useState('');
+  const [name , setName] = useState('Nome');
+  const [sobrenome , setSobrenome] = useState('Sobrenome');
+  const [idade , setIdade] = useState('Idade');
+  
 
   function mudandoName(event: React.ChangeEvent<HTMLInputElement>){
-    setName(event?.target.value);
+    setName(event.target.value);
   }
+
+  function mudandoSobrenome(event: React.ChangeEvent<HTMLInputElement>){
+    setSobrenome(event.target.value);
+  }
+
+  function mudandoIdade(event: React.ChangeEvent<HTMLInputElement>){
+    setIdade(event.target.value);
+  }
+  
   return(
     <div className="input">
       <input type="text"  value={name} onChange={mudandoName}/>
-      <p>Seu nome é: {name}</p>
+      <input type="text"  value={sobrenome} onChange={mudandoSobrenome}/>
+      <input type="text"  value={idade} onChange={mudandoIdade}/>
+      <p>Olá {name} {sobrenome},<br /> Você tem {idade} anos.</p>
     </div>
   );
 }
