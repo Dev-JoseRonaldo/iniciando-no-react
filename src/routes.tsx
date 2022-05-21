d60317da2618b7fd950f334723f8f4bc97a29c56
José Ronaldo <dev.joseronaldo@gmail.com>
2022-05-20 22:31:16 -0300

    :sparkles: feat: Criando rota geral de ERRO!

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from "./view/Home";
 import Contato from "./view/Contato";
+import Err from "./view/err";
 
 const Router: React.FC = () => {
 
   return(
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<Err />}/>
         <Route path="/" element={<Home />}/>
         <Route path="/contato" element={<Contato />}/>
       </Routes>
