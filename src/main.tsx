8f1bbbf435e0ccaef292f01fb3bd0c1f56d541f8
José Ronaldo <dev.joseronaldo@gmail.com>
2022-05-20 21:46:15 -0300

    :sparkles: feat: Renderizando conteúdo condicional

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Applist from './view/appList/Applist'
+import Condicionais from './view/condicionais/Condicionais'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Applist />
+    <Condicionais />
   </React.StrictMode>
 )
--- src/view/condicionais/Condicionais.tsx
+++ src/view/condicionais/Condicionais.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 
 const Condicionais = () => {
+  const [show,setShow] = useState(false);
+
+  function mostrar(){
+    setShow(!show);
+  }
+
   return(
     <div>
+      <button onClick={mostrar}>{show ? 'Ocultar' : 'Mostrar'}</button>
 
+      {show && <div>        
+          <h1>Conteúdo a ser Mostrado!</h1>
+        </div>}
     </div>
   );
 }
